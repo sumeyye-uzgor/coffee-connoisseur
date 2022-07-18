1cca695bb588d8dc5cfa3c487183334704be8faf
Sümeyye Üzgör <sumeyye.uzgor@venturelabs.team>
2022-07-18 13:15:40 +0300

    connect application to foursquare api and get real data

--- pages/index.js
+++ pages/index.js
@@ -4,14 +4,26 @@ import Image from "next/image";
 import Banner from "../components/banner";
 import Card from "../components/card";
 
-import coffeeStores from "../data/coffee-stores.json";
-
 import styles from "../styles/Home.module.css";
 
 export async function getStaticProps(context) {
+  const options = {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      Authorization: process.env.FOURSQUARE_API_KEY,
+    },
+  };
+  const response = await fetch(
+    "https://api.foursquare.com/v3/places/search?query=coffee&ll=40.868192%2C29.127591&limit=6",
+    options
+  );
+  const { results } = await response.json();
+  console.log(results);
+  // .catch((err) => console.error(err));
   return {
     props: {
-      coffeeStores,
+      coffeeStores: results,
     }, // will be passed to the page component as props
   };
 }
@@ -46,10 +58,13 @@ export default function Home({ coffeeStores }) {
             <div className={styles.cardLayout}>
               {coffeeStores.map((coffeeStore) => (
                 <Card
-                  key={coffeeStore.id}
-                  imgUrl={coffeeStore.imgUrl}
+                  key={coffeeStore.fsq_id}
+                  imgUrl={
+                    coffeeStore.imgUrl ||
+                    "https://images.unsplash.com/photo-1504753793650-d4a2b783c15e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80"
+                  }
                   name={coffeeStore.name}
-                  id={coffeeStore.id}
+                  id={coffeeStore.fsq_id}
                   className={styles.card}
                 />
               ))}
