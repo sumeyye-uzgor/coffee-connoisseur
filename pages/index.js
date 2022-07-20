2734a0c7625117b321985de47a2429c279b85dc1
Sümeyye Üzgör <sumeyye.uzgor@venturelabs.team>
2022-07-21 00:22:48 +0300

    create a hook named useTrackLocation to find users location

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import Image from "next/image";
 import Banner from "../components/banner";
 import Card from "../components/card";
 import fetchCoffeeStores from "../lib/coffee-stores";
+import useTrackLocation from "../hooks/use-track-location";
 
 import styles from "../styles/Home.module.css";
 
@@ -16,8 +17,12 @@ export async function getStaticProps(context) {
   };
 }
 export default function Home({ coffeeStores }) {
+  const { handleTrackLocation, latLong, locationErrorMsg, isFindingLocation } =
+    useTrackLocation();
+  console.log({ latLong });
+  console.log({ locationErrorMsg });
   const handleOnBannerButtonClick = () => {
-    console.log("clikced");
+    handleTrackLocation();
   };
   return (
     <div className={styles.container}>
@@ -29,9 +34,10 @@ export default function Home({ coffeeStores }) {
 
       <main className={styles.main}>
         <Banner
-          buttonText={"View stores nearby"}
+          buttonText={isFindingLocation ? "Locating..." : "View stores nearby"}
           handleOnClick={handleOnBannerButtonClick}
         />
+        {locationErrorMsg && <p>Something went wrong... {locationErrorMsg}</p>}
         <div className={styles.heroImage}>
           <Image
             src="/static/hero-image.png"
@@ -40,27 +46,29 @@ export default function Home({ coffeeStores }) {
             height={400}
           />
         </div>
-        {coffeeStores.length > 0 ? (
-          <>
-            <h2 className={styles.heading2}>Toronto coffee stores</h2>
-            <div className={styles.cardLayout}>
-              {coffeeStores.map((coffeeStore) => (
-                <Card
-                  key={coffeeStore.id}
-                  imgUrl={
-                    coffeeStore?.imgUrl ||
-                    "https://images.unsplash.com/photo-1504753793650-d4a2b783c15e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80"
-                  }
-                  name={coffeeStore.name}
-                  id={coffeeStore.id}
-                  className={styles.card}
-                />
-              ))}
-            </div>
-          </>
-        ) : (
-          <div>No data fiound</div>
-        )}
+        <div className={styles.sectionWrapper}>
+          {coffeeStores.length > 0 ? (
+            <>
+              <h2 className={styles.heading2}>Toronto coffee stores</h2>
+              <div className={styles.cardLayout}>
+                {coffeeStores.map((coffeeStore) => (
+                  <Card
+                    key={coffeeStore.id}
+                    imgUrl={
+                      coffeeStore?.imgUrl ||
+                      "https://images.unsplash.com/photo-1504753793650-d4a2b783c15e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80"
+                    }
+                    name={coffeeStore.name}
+                    id={coffeeStore.id}
+                    className={styles.card}
+                  />
+                ))}
+              </div>
+            </>
+          ) : (
+            <div>No data fiound</div>
+          )}
+        </div>
       </main>
     </div>
   );
