41bace064d849cd769cdbbb307d3401ed58bce27
Sümeyye Üzgör <sumeyye.uzgor@venturelabs.team>
2022-07-24 11:28:24 +0300

    create context for application

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,40 @@
-import '../styles/globals.css'
+import { createContext, useReducer } from "react";
+import "../styles/globals.css";
 
+const StoreContext = createContext();
+const ACTION_TYPES = {
+  SET_LAG_LONG: "SET_LAG_LONG",
+  SET_COFFEE_STORES: "SET_COFFEE_STORES",
+};
+const storeReducer = (state, action) => {
+  switch (action.type) {
+    case ACTION_TYPES.SET_COFFEE_STORES:
+      return { ...state, coffeeStores: action.payload.coffeeStores };
+    case ACTION_TYPES.SET_LAG_LONG:
+      return { ...state, latLong: action.payload.latLong };
+    default:
+      throw new Error(`Unhandled action type: ${action.type}`);
+  }
+};
+const StoreProvider = ({ children }) => {
+  const initialState = {
+    latLong: "",
+    coffeeStores: [],
+  };
+
+  const [state, dispatch] = useReducer(storeReducer, initialState);
+  return (
+    <StoreContext.Provider value={{ state, dispatch }}>
+      {children}
+    </StoreContext.Provider>
+  );
+};
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <StoreProvider>
+      <Component {...pageProps} />
+    </StoreProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
