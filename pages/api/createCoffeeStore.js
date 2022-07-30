8bac7b2d9b2d52b9764b66f65062149a1c1ec31e
Sümeyye Üzgör <sumeyye.uzgor@venturelabs.team>
2022-07-30 14:10:30 +0300

    create createCoffeeStore api for backend

--- pages/api/createCoffeeStore.js
+++ pages/api/createCoffeeStore.js
@@ -1,18 +1,46 @@
-import Airtable from "airtable";
+import { table, getMinifiedRecords } from "./airtable";
 
-const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(
-  process.env.AIRTABLE_BASE_KEY
-);
-const table = base("coffee-stores");
-
-const createCoffeeStore = (req, res) => {
-  // res.status(200).json({ message: "Hi There" });
+const createCoffeeStore = async (req, res) => {
   if (req.method === "POST") {
-    res.status(200).json({ message: "Hi there!" });
-  } else {
-    res
-      .status(400)
-      .json({ message: "Only POST requests valid for this endpoint" });
+    //find a record
+    const { id, name } = req.body;
+
+    try {
+      if (id) {
+        const findCoffeeStoreRecords = await table
+          .select({
+            filterByFormula: `id=${id}`,
+          })
+          .firstPage();
+
+        if (findCoffeeStoreRecords.length !== 0) {
+          res.status(200).json(getMinifiedRecords(findCoffeeStoreRecords));
+        } else {
+          //create a record
+          if (name) {
+            const createRecords = await table.create([
+              {
+                fields: req.body,
+              },
+            ]);
+
+            res.status(200).json(getMinifiedRecords(createRecords));
+          } else {
+            res.status(422).json({
+              message: "name property is necessary to create a new store",
+            });
+          }
+        }
+      } else {
+        res.status(422).json({
+          message: "id property is necessary to create a new store",
+        });
+      }
+    } catch (err) {
+      res
+        .status(500)
+        .json({ message: "Error creating or finding a store", err });
+    }
   }
 };
 
